ba36d88c361c1208d9816b7b006e4107d04bc517
gaojiaqi <1403218804@qq.com>
2019-09-04 17:57:19 +0800

    message

---
+++ framework/src/main/java/com/harmonycloud/framework/adapter/inbound/message/MessageConsumer.java
@@ -0,0 +1,9 @@
+package com.harmonycloud.framework.adapter.inbound.message;
+
+/**
+ * @Usage:
+ * @Author: gjq
+ * @Date: 2019-09-04 17:55
+ */
+public class MessageConsumer {
+}
---
+++ framework/src/main/java/com/harmonycloud/framework/adapter/outbound/message/MessagePROVIDER.java
@@ -0,0 +1,9 @@
+package com.harmonycloud.framework.adapter.outbound.message;
+
+/**
+ * @Usage:
+ * @Author: gjq
+ * @Date: 2019-09-04 17:56
+ */
+public class MessagePROVIDER {
+}
