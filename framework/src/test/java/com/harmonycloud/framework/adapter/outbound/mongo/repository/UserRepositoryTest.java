012709dc73341c261faeb14f708c93d4b054f1cd
gaojiaqi <1403218804@qq.com>
2019-09-05 16:28:38 +0800

    集成mongodb单元测试框架

--- framework/src/test/java/com/harmonycloud/framework/adapter/outbound/mongo/repository/UserRepositoryTest.java
+++ framework/src/test/java/com/harmonycloud/framework/adapter/outbound/mongo/repository/UserRepositoryTest.java
@@ -1,17 +1,14 @@
 package com.harmonycloud.framework.adapter.outbound.mongo.repository;
 
+import com.harmonycloud.common.BaseMongoTest;
 import com.harmonycloud.framework.adapter.outbound.mongo.entity.User;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class UserRepositoryTest {
+
+public class UserRepositoryTest extends BaseMongoTest {
     @Autowired
     private UserRepository userRepository;
 
